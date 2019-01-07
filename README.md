5d0edfa2b9d2b6a2f7580864f2daa73e37fe7f19
Trung Nguyen <puritatemcordis@gmail.com>
2019-01-06 22:39:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Yelp-Camp
+> ME(A)N stack web application project from the Udemy course - [The Web Developer Bootcamp by Colt Steele](https://www.udemy.com/the-web-developer-bootcamp/)
+
+## Live Demo
+Deployed web application: https://yelpcamp-puritatemcordis.herokuapp.com/
+
+### Features
+* Authentication
+* Authorization
+* CRUD Functionality
+* RESTful API
+* Responsive Web Design
+
+### Dependencies
+* Front-end
+    * [ejs](https://ejs.co/)
+    * [Bootstrap 3.3](https://getbootstrap.com/docs/3.3/)
+* Back-end
+    * [express](https://expressjs.com/)
+    * [mongoDB](https://www.mongodb.com/)
+    * [mongoose](https://mongoosejs.com/)
+    * [passport](http://www.passportjs.org/)
+    * [passport-local](http://www.passportjs.org/packages/passport-local/)
+    * [express-session](https://github.com/expressjs/session#express-session)
+    * [connect-flash](https://github.com/jaredhanson/connect-flash)
+* Platforms
+    * [Heroku](https://dashboard.heroku.com/)
+    * [mLab](https://mlab.com/home)
+
+### Getting Started
+1. Clone app - `git clone https://github.com/puritatemcordis/Yelp-Camp.git`
+2. Start the application - `node app.js`
+3. Browse to http://localhost:3000
+
---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var flash = require("connect-flash");
+var LocalStrategy = require("passport-local");
+var methodOverride = require("method-override");
+
+//models
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var User = require("./models/user");
+var seedDB = require("./seeds");
+
+//requiring routes
+var commentRoutes = require("./routes/comments");
+var campgroundRoutes = require("./routes/campgrounds");
+var indexRoutes = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/yelp_camp_v10", {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+//seedDB(); //seed the database
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Bear wins cutest cat!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//middleware that runs for every single route (navbar)
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
+
+//app.use for routes
+//first argument appends "..." to all routes
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments",commentRoutes);
+
+app.listen(process.env.PORT || 3000, process.env.IP);
+console.log("The YelpCamp Server Has Started");
